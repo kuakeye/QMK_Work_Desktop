af1c5df17fe0d95c5bed87a497c90d801f6b12b1
kuakeye <kuakeye@gmail.com>
2020-03-09 08:48:33 -0500

    Modify Dactylv2_Lightcycle Stuff
    Changed Dactyl.h to fill out all possible row/columns
    Changed rev2.h to fill out all possible row/columns

--- keyboards/Dactylv2_Lightcycle/Dactyl.h
+++ keyboards/Dactylv2_Lightcycle/Dactyl.h
@@ -15,16 +15,16 @@
     L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, \
     L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25, \
          L30, L31, L32, L33, L34,           R31, R32, R33, R34, R35, \
-              L40, L41, L42, L43,           R42, R43, R44, R45, \
-                        L50, L51,           R54, R55 \
+L40, L41, L42, L43, KC_1, KC_2,          KC_7, KC_8,            R42, R43, R44, R45, \
+   L50, L51, KC_3,  KC_4, KC_5, KC_6, KC_9,  KC_0, KC_A, KC_B,  R54, R55  \
     ) \
     KEYMAP( \
         KC_##L00, KC_##L01, KC_##L02, KC_##L03, KC_##L04, KC_##L05,             KC_##R00, KC_##R01, KC_##R02, KC_##R03, KC_##R04, KC_##R05, \
         KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, KC_##L15,             KC_##R10, KC_##R11, KC_##R12, KC_##R13, KC_##R14, KC_##R15, \
         KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, KC_##L25,             KC_##R20, KC_##R21, KC_##R22, KC_##R23, KC_##R24, KC_##R25, \
                   KC_##L30, KC_##L31, KC_##L32, KC_##L33, KC_##L34,             KC_##R31, KC_##R32, KC_##R33, KC_##R34, KC_##R35, \
-                            KC_##L40, KC_##L41, KC_##L42, KC_##L43,             KC_##R42, KC_##R43, KC_##R44, KC_##R45, \
-                                                KC_##L50, KC_##L51,             KC_##R54, KC_##R55 \
+KC_##L40, KC_##L41, KC_##L42, KC_##L43, KC_##KC_1, KC_##KC_2,          KC_##KC_7, KC_##KC_8,            KC_##R42, KC_##R43, KC_##R44, KC_##R45, \
+   KC_##L50, KC_##L51, KC_##KC_3,  KC_##KC_4, KC_##KC_5, KC_##KC_6, KC_##KC_9,  KC_##KC_0, KC_##KC_A, KC_##KC_B,  KC_##R54, KC_##R55  \
     )
 
     
